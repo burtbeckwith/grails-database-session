4b4eb476125f7b09a950e047d946e76d23cdd1f1
Burt Beckwith <beckwithb@vmware.com>
2012-02-22 04:12:14 -0500

    v1.1.1 release

--- DatabaseSessionGrailsPlugin.groovy
+++ DatabaseSessionGrailsPlugin.groovy
@@ -5,7 +5,7 @@ import grails.util.Metadata
 import org.springframework.web.filter.DelegatingFilterProxy
 
 class DatabaseSessionGrailsPlugin {
-	String version = '1.1'
+	String version = '1.1.1'
 	String grailsVersion = '1.3.3 > *'
 	String title = 'Database Session Plugin'
 	String author = 'Burt Beckwith'
